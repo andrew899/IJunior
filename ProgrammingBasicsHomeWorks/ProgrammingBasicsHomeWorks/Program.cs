f9f2027b277d2a40dcd2ffccd1cf8c4870111744
Andrew Kovalenko <andrew8999@gmail.com>
2021-03-02 13:52:51 +0200

    Made project

---
+++ ProgrammingBasicsHomeWorks/ProgrammingBasicsHomeWorks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingBasicsHomeWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
