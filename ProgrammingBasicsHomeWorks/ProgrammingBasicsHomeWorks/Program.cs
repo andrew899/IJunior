f19b1d89f8d3b56de1ae22e23767ad4bafee2e9e
Andrew Kovalenko <andrew8999@gmail.com>
2021-03-02 14:07:48 +0200

    Variables

--- ProgrammingBasicsHomeWorks/ProgrammingBasicsHomeWorks/Program.cs
+++ ProgrammingBasicsHomeWorks/ProgrammingBasicsHomeWorks/Program.cs
@@ -6,7 +6,29 @@ namespace ProgrammingBasicsHomeWorks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Variables
+            {
+                // переменные без смысла (без кода) трудно называть осмысленно
+                int score;
+                uint lederboardPosition;
+                float result;
+
+                string name;
+                string surname;
+
+                char simbol;
+
+                double money;
+
+                uint serialNumber;
+                uint postCode;
+
+                bool isNumber;
+            }
+
+            
+
         }
+
     }
 }
