1aaeeb2ca868872c97d3042334f3a23d2824f67e
Andrew Kovalenko <andrew8999@gmail.com>
2021-03-24 09:29:13 +0200

    Update Program.cs

--- ProgrammingBasicsHomeWorks/ProgrammingBasicsHomeWorks/Program.cs
+++ ProgrammingBasicsHomeWorks/ProgrammingBasicsHomeWorks/Program.cs
@@ -12,17 +12,21 @@ namespace ProgrammingBasicsHomeWorks
                 int playerMoney = 0;
                 int priceCrystal = random.Next(1, 101);
 
-                int boughtCrystal = 0;
+                int demandToBuy = 0;
+                int fullPrice = 0;
 
                 Console.WriteLine("Welcom to our shop!");
                 Console.WriteLine($"One crystal costs: {priceCrystal}");
                 Console.Write("How much money do you have? ");
                 playerMoney =  Int32.Parse(Console.ReadLine());
 
-                boughtCrystal = playerMoney / priceCrystal;
-                playerMoney = playerMoney % priceCrystal;
+                Console.Write("How many crystals do you want to buy? ");
+                demandToBuy =  Int32.Parse(Console.ReadLine());
 
-                Console.WriteLine($"You bought: {boughtCrystal}.");
+                fullPrice = demandToBuy * priceCrystal;
+                playerMoney = playerMoney - fullPrice;
+
+                Console.WriteLine($"You bought: {demandToBuy}.");
                 Console.WriteLine($"Money left: {playerMoney}.");
             }
         }
