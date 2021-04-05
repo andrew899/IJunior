9fb58b3ed9102b3d67311f136b37c45d2b498c39
Andrew Kovalenko <andrew8999@gmail.com>
2021-04-05 08:48:05 +0300

    new project for second home work

---
+++ ProgrammingBasicsHomeWorks/ProgrammingBasicsHomeWorks_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProgrammingBasicsHomeWorks_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userText = "";
+            int amountOfRepeat = 0;
+
+            Console.Write("Write your text: ");
+            userText = Console.ReadLine();
+            
+            Console.Write("How many times repeat: ");
+            amountOfRepeat = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < amountOfRepeat; i++)
+            {
+                Console.WriteLine(i + ". " + userText);
+            }
+        }
+    }
+}
