ade051ba1686c8f7c814084e4c944667da86d59e
Andrew Kovalenko <andrew8999@gmail.com>
2021-04-11 14:27:35 +0300

    Update Program.cs

--- ProgrammingBasicsHomeWorks/ProgrammingBasicsHomeWorks/Program.cs
+++ ProgrammingBasicsHomeWorks/ProgrammingBasicsHomeWorks/Program.cs
@@ -6,43 +6,22 @@ namespace ProgrammingBasicsHomeWorks
     {
         static void Main(string[] args)
         {
-            string name = "Alexey";
+            string userInput = "Alexey";
             char symbol = '%';
 
             Console.Write("Enter your string: ");
-            name = Console.ReadLine();
+            userInput = Console.ReadLine();
 
             Console.Write("Enter your symbol: ");
             symbol = Console.ReadKey().KeyChar;
             Console.WriteLine();
 
-            int leftborder = 1;
-            int rightborder = 1;
-            int leftPadinng = 1;
-            int rightPadinng = 1;
+            string borderLine = $"{symbol}" + $"{symbol}" + new string(symbol, userInput.Length) + $"{symbol}" + $"{symbol}";
+            string userInputLine = $"{symbol}" + " " + userInput + " " + $"{symbol}";
 
-            int lineAmount = 3;
-            int lineLenth = leftborder + leftPadinng + name.Length + rightPadinng + rightborder;
-
-            for (int i = 0; i < lineAmount; i++)
-            {
-                for (int j = 0; j < lineLenth; j++)
-                {
-                    if (i == 0 || i == (lineAmount - 1))
-                        Console.Write(symbol);
-                    else if (j < rightborder || j == lineLenth - 1)
-                        Console.Write(symbol);
-                    else if (j == leftborder + leftPadinng)
-                    {
-                        Console.Write(name);
-                        j += name.Length - 1;
-                    }
-                    else
-                        Console.Write(" ");
-                }
-                Console.WriteLine();
-            }
-
+            Console.WriteLine(borderLine);
+            Console.WriteLine(userInputLine);
+            Console.WriteLine(borderLine);
         }
     }
 }
